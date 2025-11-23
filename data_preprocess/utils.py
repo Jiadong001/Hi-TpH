4a5de612175640ed0798750133e51e3741f5742d
Jiadong001 <ljd_pb19000334@mail.ustc.edu.cn>
2025-11-24 00:12:32 +0800

    update

--- data_preprocess/utils.py
+++ data_preprocess/utils.py
@@ -2,6 +2,30 @@ import os
 import matplotlib.pyplot as plt
 
 
+def save_csv(df, path, filename):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    df.to_csv(path+filename, index=False)
+    print(f"File {path+filename} has been successfully saved.")
+
+
+def plot_non_empty_percentage(df, database):
+    non_empty_counts = df.count()
+    total_rows = len(df)
+    non_empty_percentages = (non_empty_counts / total_rows) * 100
+
+    plt.figure(figsize=(10, 6))
+    non_empty_percentages.plot(kind='bar')
+    plt.title(f'{database}: Percentage of Non-Empty Elements in Each Column')
+    plt.xlabel('Columns')
+    plt.ylabel('Percentage (%)')
+    plt.xticks(rotation=75)
+    plt.hlines(100, 0-0.5, len(df.columns)+0.5, color='black', linestyles='dashed')
+    plt.tight_layout()
+    plt.show()
+
+
 # Reconstruct TCR variable sequence
 def reconstruct_vseq(data):
     """
@@ -26,27 +50,3 @@ def reconstruct_vseq(data):
         return vgene_seq[:vgene_seq_end] + cdr3 + jgene_seq[jgene_seq_start:]
     else:
         return '(UNK)'
-
-
-def save_csv(df, path, filename):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    df.to_csv(path+filename, index=False)
-    print(f"File {path+filename} has been successfully saved.")
-
-
-def plot_non_empty_percentage(df, database):
-    non_empty_counts = df.count()
-    total_rows = len(df)
-    non_empty_percentages = (non_empty_counts / total_rows) * 100
-
-    plt.figure(figsize=(10, 6))
-    non_empty_percentages.plot(kind='bar')
-    plt.title(f'{database}: Percentage of Non-Empty Elements in Each Column')
-    plt.xlabel('Columns')
-    plt.ylabel('Percentage (%)')
-    plt.xticks(rotation=75)
-    plt.hlines(100, 0-0.5, len(df.columns)+0.5, color='black', linestyles='dashed')
-    plt.tight_layout()
-    plt.show()
